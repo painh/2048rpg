4dc0a9e0bc08dd214ccc9e70b8992e6e4ae4bc78
sihyun <gthpgth@gmail.com>
2015-12-25 17:11:12 +0900

    global 추가

--- src/app.js
+++ src/app.js
@@ -1,23 +1,4 @@
 /* global cc */
-var TILE_SIZE = 32;
-var MOVE_STEP = 8;
-function randomRange(min, max)
-{
-	return Math.floor((Math.random() * max) + min);	
-};
-
-function hpToColor(hp)
-{
-	switch(hp)
-	{
-		case -1 : return cc.color(64, 64, 64);
-		case  0 : return cc.color(128, 128, 128);
-		case  1 : return cc.color(160, 160, 160);
-		case  2 : return cc.color(228, 228, 228);
-		case  3 : return cc.color(255, 255, 255);
-	}; 
-}
-
 var HelloWorldScene = cc.Scene.extend({
 	prevMovedCnt : 0,
 	mapLayer : null,
